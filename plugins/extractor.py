79f196a035004e5ec84b46d8bd3edb02300ece78
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-10 11:05:09 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -1,7 +1,10 @@
-from pyrogram import filters, Client
+from pyrogram import filters
+from pyrogram import Client
+from plugins import start 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from plugins import merge
-from plugins import audio 
+from plugins import audio
+from plugins import merge 
+from helper import download 
 from config import Config
 
 @Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
@@ -9,22 +12,21 @@ async def confirm_dwnld(client, message):
     media = message
     filetype = media.document or media.video or media.audio
 
-    # Check the MIME type
-    if filetype.mime_type.startswith("video/") or filetype.mime_type.startswith("audio/"):
-        if filetype.mime_type.startswith("video/"):
-            keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
-                 InlineKeyboardButton(text="Remove Audio 🎧", callback_data="handle_remove_audio")],
-                [InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
-                 InlineKeyboardButton(text="audio+audio 🎵", callback_data="set_merge_audio")],
-                [InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
-                 InlineKeyboardButton(text="CANCEL", callback_data="close")]
-            ])
-        
+    if filetype.mime_type.startswith("video/"):
         await message.reply_text(
             "**What do you want me to do 🤔**",
             quote=True,
-            reply_markup=keyboard
+            reply_markup=InlineKeyboardMarkup([
+                [ InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
+                  InlineKeyboardButton(text="Remove Audio🎧", callback_data="handle_remove_audio")
+                ],
+                [ InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
+                  InlineKeyboardButton(text="audio+audio🎵", callback_data="set_merge_audio")
+                ],
+                [ InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
+                  InlineKeyboardButton(text="CANCEL", callback_data="close")
+                ]
+            ])
         )
     else:
         await message.reply_text(
