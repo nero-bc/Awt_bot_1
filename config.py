46ef1e8d4dd1f08c5395d2ac8860380c7c966bef
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-10 07:57:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,32 @@
 import os, time
 
 class Config(object):
-    #Audio-_edit_bot client Config 
+    # Audio-_edit_bot client Config 
     API_ID = os.getenv("API_ID", "21740783")
     API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7496680438:AAHyEZDGnIoARpfywrzQOhB27un9pja49p4")
-    # database config
-    DB_NAME = os.environ.get("DB_NAME","Speedwolf1")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Speedwolf1:speedwolf24689@cluster0.rgfywsf.mongodb.net/")
-
-
-    # other configs
+    
+    # Database config
+    DB_NAME = os.environ.get("DB_NAME", "Speedwolf1")     
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://Speedwolf1:speedwolf24689@cluster0.rgfywsf.mongodb.net/")
+    
+    # Other configs
     BOT_UPTIME  = time.time()
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/feb6dd0a1cb8576943c0f.jpg")
     
-    # wes response configuration     
+    # Web response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
     
-    ADMIN   = [int(os.environ.get("ADMIN", "6299192020"))
-
+    ADMIN   = [int(os.environ.get("ADMIN", "6299192020"))]  # Closed the square bracket here
     
 class Txt(object):
     
-    START_TXT = """Hello Friends i am The Advance 📹Video Editor bot i have special Features like "Audio remover" and "video trimmer" 
+    START_TXT = """Hello Friends I am The Advanced 📹Video Editor bot. I have special features like "Audio remover" and "Video trimmer."
     
 </a>"\n Bot Is Made By @Anime_Warrior_Tamil"</b>"""
     
-    ABOUT_TXT = f"""<b>😈 My Name :</b> <a href='https://t.me/Gjjbsrijjb_bot'>Video editor bot ⚡</a>
+    ABOUT_TXT = f"""<b>😈 My Name :</b> <a href='https://t.me/Gjjbsrijjb_bot'>Video Editor Bot ⚡</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
@@ -38,20 +37,20 @@ class Txt(object):
 
 
     HELP_TXT = """
- <b><u>Video Editor bot Commands</u></b>
+<b><u>Video Editor Bot Commands</u></b>
   
-<b>•»</b> /start Use this command to Check bot is alive ✅.
-<b>•»</b> /remove_audio Uꜱᴇ This Command to remove audio.
-<b>•»</b> /trim_video Use this command to Trim video.
+<b>•»</b> /start Use this command to check if the bot is alive ✅.
+<b>•»</b> /remove_audio Use this command to remove audio.
+<b>•»</b> /trim_video Use this command to trim video.
 
-✏️ <b><u>Hᴏᴡ Tᴏ use bot</u></b>
+✏️ <b><u>How To Use the Bot</u></b>
 
-<b>•»</b> Reply to a video to remove audio or trim video          
+<b>•»</b> Reply to a video to remove audio or trim video.          
 
-ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/AWT_bots_developer>𝘽𝙤𝙩 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧</a>
+ℹ️ Any other help, contact: <a href=https://t.me/AWT_bots_developer>Bot Developer</a>
 """
 
-#⚠️ Dᴏɴ'ᴛ Rᴇᴍᴏᴠᴇ Oᴜʀ Cʀᴇᴅɪᴛꜱ @ILLGELA_DEVELOPER🙏🥲
+    # ⚠️ Don't Remove Our Credits @ILLGELA_DEVELOPER🙏🥲
     DEV_TXT = """hiiiiiiiiiii"""
     
     PROGRESS_BAR = """\n
@@ -59,7 +58,3 @@ class Txt(object):
 <b>⏳️ Done</b> : {0}%
 <b>🚀 Speed</b> : {3}/s
 <b>⏰️ ETA</b> : {4} """
-
-
-
-#This bot was created by Awt botz
