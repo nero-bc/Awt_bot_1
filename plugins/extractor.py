4fa1cd60b39a3edc67c1d02fc88bfa9c984aea08
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-10 16:24:04 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -7,8 +7,12 @@ from config import Config
 @Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
 async def confirm_dwnld(client, message):
     media = message
-    filetype = media.document or media.video or media.audio
-
+    filetype = media.document or media.video 
+    await message.reply_text(
+        "**What do you want me to do with this File 🤔**",
+        quote=True,
+        reply_markup=keyboard
+    )
     # Check the MIME type
     if filetype.mime_type.startswith("video/") or filetype.mime_type.startswith("audio/"):
         if filetype.mime_type.startswith("video/"):
@@ -16,21 +20,11 @@ async def confirm_dwnld(client, message):
                 [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
                  InlineKeyboardButton(text="Remove Audio 🎧", callback_data="handle_remove_audio")],
                 [InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
-                 InlineKeyboardButton(text="audio+audio 🎵", callback_data="merge_audio")],
-                [InlineKeyboardButton(text="Video+audio 📹", callback_data="merge_video"),
+                 InlineKeyboardButton(text="audio+audio 🎵", callback_data="set_merge_audio")],
+                [InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
                  InlineKeyboardButton(text="CANCEL", callback_data="close")]
             ])
-        elif filetype.mime_type.startswith("audio/"):
-            keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="audio+audio 🎵", callback_data="merge_audio")],
-                [InlineKeyboardButton(text="CANCEL", callback_data="close")]
-            ])
         
-        await message.reply_text(
-            "**What do you want me to do 🤔**",
-            quote=True,
-            reply_markup=keyboard
-        )
     else:
         await message.reply_text(
             "Invalid Media",
