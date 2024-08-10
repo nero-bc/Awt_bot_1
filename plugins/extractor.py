9db1136a751bb1265c37197156f38c6c79716107
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-10 08:22:50 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -1,10 +1,6 @@
-from pyrogram import filters
-from pyrogram import Client
-from plugins import start 
+from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from plugins import audio
-from plugins import merge 
-from helper import download 
+from merge import set_merge_audio, set_merge_video  # Import from merge.py
 from config import Config
 
 @Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
@@ -17,19 +13,16 @@ async def confirm_dwnld(client, message):
             "**What do you want me to do 🤔**",
             quote=True,
             reply_markup=InlineKeyboardMarkup([
-                [ InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
-                  InlineKeyboardButton(text="Remove Audio🎧", callback_data="handle_remove_audio")
-                ],
-                [ InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
-                  InlineKeyboardButton(text="audio+audio🎵", callback_data="set_merge_audio")
-                ],
-                [ InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
-                  InlineKeyboardButton(text="CANCEL", callback_data="close")
-                ]
+                [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
+                 InlineKeyboardButton(text="Remove Audio 🎧", callback_data="handle_remove_audio")],
+                [InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
+                 InlineKeyboardButton(text="audio+audio 🎵", callback_data="merge_audio")],
+                [InlineKeyboardButton(text="Video+audio 📹", callback_data="merge_video"),
+                 InlineKeyboardButton(text="CANCEL", callback_data="close")]
             ])
         )
     else:
         await message.reply_text(
             "Invalid Media",
             quote=True
-        )
+                 )
