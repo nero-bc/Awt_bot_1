fa973bebae8d2398fb692daea721755ef0d644b0
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-11 09:17:35 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -2,21 +2,20 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins import merge
 from plugins import audio   # Import from merge.py
+from helper import download 
 from config import Config
 
-@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
+
+@Client.on_message(filters.private & (filters.document | filters.video))
 async def confirm_dwnld(client, message):
     media = message
-    filetype = media.document or media.video 
-    await message.reply_text(
-        "**What do you want me to do with this File 🤔**",
-        quote=True,
-        reply_markup=keyboard
-    )
-    # Check the MIME type
-    if filetype.mime_type.startswith("video/") or filetype.mime_type.startswith("audio/"):
-        if filetype.mime_type.startswith("video/"):
-            keyboard = InlineKeyboardMarkup([
+    filetype = media.document or media.video
+
+    if filetype.mime_type.startswith("video/"):
+        await message.reply_text(
+            "**What do you want me to do 🤔**",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
                  InlineKeyboardButton(text="Remove Audio 🎧", callback_data="handle_remove_audio")],
                 [InlineKeyboardButton(text="Trim Video ✂️", callback_data="handle_trim_video"),
@@ -24,9 +23,8 @@ async def confirm_dwnld(client, message):
                 [InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
                  InlineKeyboardButton(text="CANCEL", callback_data="close")]
             ])
-        
     else:
         await message.reply_text(
             "Invalid Media",
             quote=True
-                 )
+)
