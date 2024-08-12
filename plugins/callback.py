c028c5f9057b38ab838e56d9097af7f197eaf04a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-12 14:28:10 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -4,7 +4,6 @@ from plugins.merge_audio import merge_audios  # Import the merge_audio function
 from plugins.merge_video import set_merge_video  # Import from merge.py
 from plugins import start, audio
 from helper.progress import PRGRS
-from plugins.audio_trim import handle_trim_audio
 from helper.tools import clean_up
 from config import Config, Txt
 from plugins.audio import handle_remove_audio
