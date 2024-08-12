44236fa85409108ed21c99a4d851a7d5fd9dbee4
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-12 14:12:42 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -7,14 +7,14 @@ from helper import download
 from config import Config
 
 
-@Client.on_message(filters.private & (filters.document | filters.video))
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
 async def confirm_dwnld(client, message):
     media = message
-    filetype = media.document or media.video
+    filetype = media.document or media.video or media.audio
 
     if filetype.mime_type.startswith("video/"):
         await message.reply_text(
-            "**What do you want me to do 🤔**",
+            "**What do you want me to do with this file 🤔**",
             quote=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
@@ -25,6 +25,16 @@ async def confirm_dwnld(client, message):
                  InlineKeyboardButton(text="CANCEL", callback_data="close")]
             ])
         )
+
+    if filetype.mime_type.startswith("audio/"):
+        await message.reply_text("what do you want me to do with this audio file 🤨")
+            quote=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text="Audio+audio🎵", callback_data="set_merge_audio"),
+                 InlineKeyboardButton(text="Trim Audio ✂️", callback_data="handle_trim_audio")],
+                [InlineKeyboardButton(text="close❌", callback_data="close")]
+                )
+               
     else:
         await message.reply_text(
             "Invalid Media",
